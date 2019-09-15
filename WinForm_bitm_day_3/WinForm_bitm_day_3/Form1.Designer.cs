b074faafacfcfe612e8c14ecd7f2fe60ddb69b78
brinta sabit <brintasabit@gmail.com>
2019-09-15 13:08:50 +0600

    15sep19bitm

---
+++ WinForm_bitm_day_3/WinForm_bitm_day_3/Form1.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace WinForm_bitm_day_3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.showButton = new System.Windows.Forms.Button();
+            this.inputTextBox = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.Display = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // showButton
+            // 
+            this.showButton.Location = new System.Drawing.Point(332, 92);
+            this.showButton.Name = "showButton";
+            this.showButton.Size = new System.Drawing.Size(75, 23);
+            this.showButton.TabIndex = 0;
+            this.showButton.Text = "Show";
+            this.showButton.UseVisualStyleBackColor = true;
+            this.showButton.Click += new System.EventHandler(this.showButton_Click);
+            // 
+            // inputTextBox
+            // 
+            this.inputTextBox.Location = new System.Drawing.Point(120, 94);
+            this.inputTextBox.Name = "inputTextBox";
+            this.inputTextBox.Size = new System.Drawing.Size(173, 20);
+            this.inputTextBox.TabIndex = 1;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(120, 144);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(173, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // Display
+            // 
+            this.Display.Location = new System.Drawing.Point(332, 141);
+            this.Display.Name = "Display";
+            this.Display.Size = new System.Drawing.Size(75, 23);
+            this.Display.TabIndex = 0;
+            this.Display.Text = "Show";
+            this.Display.UseVisualStyleBackColor = true;
+            this.Display.Click += new System.EventHandler(this.showButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.inputTextBox);
+            this.Controls.Add(this.Display);
+            this.Controls.Add(this.showButton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button showButton;
+        private System.Windows.Forms.TextBox inputTextBox;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button Display;
+    }
+}
+
---
+++ WinForm_bitm_day_3/WinForm_bitm_day_3/Form1.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinForm_bitm_day_3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void showButton_Click(object sender, EventArgs e)
+        {
+
+            /*string[] name = new string[5];
+            name[0] = "Brinta";
+            name[1] = "Brinta";
+            name[2] = "Brinta";
+            name[3] = "Brinta";
+            name[4] = "Brinta";
+            int[] age = new int[] {23,24,25,26,27};
+            string showInfo=" ";
+            for (int i = 0; i <= 4; i++)
+            {
+                showInfo +=("\nName: " + name[i] + "\nAge: " + age[i]);
+
+            }
+            MessageBox.Show(showInfo);*/
+
+            /* int[] number = new int[10];
+             number[0] = 1;
+             number[1] = 1;
+             number[2] = 2;
+             number[3] = 3;
+             number[4] = 4;
+             number[5] = 5;
+             number[6] = 6;
+             number[7] = 7;
+             number[8] = 8;
+             number[9] = 9;
+             // number[0] = 1;
+             string showNumber = "";
+             for (int i=0;i<10;i++)
+             {
+                 showNumber += (+number[i]);
+               // Array.Reverse(number);
+             }
+
+             MessageBox.Show(showNumber);
+            // MessageBox.Show(Array.Reverse(number));
+
+     */
+
+           // int[] number = new int[100];
+            /*number[0] = 2;
+            number[1] = 5;
+            number[2] = 7;*/
+            string array = inputTextBox.Text;
+            int[] number = Convert.ToInt32(inputTextBox.Text);
+            for (int i = 0; i < n; i++)
+            {
+                showNumber += (+number[i]);
+                // Array.Reverse(number);
+            }
+            MessageBox.Show(showNumber);
+
+
+
+
+        }
+    }
+}
